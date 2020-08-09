35ce29fb2c9145c33bebeda15116d7e5725bcd02
Pablo Miralles <pmirallesr@gmail.com>
2020-08-09 17:50:01 +0200

    Initial commit

---
+++ mod_problems/direct_pl2pl_mod.py
@@ -0,0 +1,167 @@
+from pykep.trajopt._direct import _direct_base
+import pykep as pk
+import numpy as np
+
+defaults = {
+    "w_mass": 0.5, "w_tof":0.5}
+
+class direct_pl2pl(_direct_base):
+    """Represents a direct transcription transfer between solar system planets.
+
+    This problem works by manipulating the starting epoch t0, the transfer time T the final mass mf and the controls 
+    The dicision vector is::
+
+        z = [t0, T, mf, Vxi, Vyi, Vzi, Vxf, Vyf, Vzf, controls]
+    """
+
+    def __init__(self,
+                 p0="earth",
+                 pf="mars",
+                 mass=1000,
+                 thrust=0.3,
+                 isp=3000,
+                 nseg=20,
+                 t0=[500, 1000],
+                 tof=[200, 500],
+                 vinf_dep=1e-3,
+                 vinf_arr=1e-3,
+                 hf=False,
+                 **kwargs):
+        """Initialises a direct transcription orbit to orbit problem.
+
+        Args:
+            - p0 (``str``): Departure planet name. (will be used to construct a planet.jpl_lp object)
+            - pf (``str``): Arrival planet name. (will be used to construct a planet.jpl_lp object)
+            - mass (``float``, ``int``): Spacecraft wet mass [kg].
+            - thrust (``float``, ``int``): Spacecraft maximum thrust [N].
+            - isp (``float``, ``int``): Spacecraft specific impulse [s].
+            - nseg (``int``): Number of colocation nodes.
+            - t0 (``list``): Launch epochs bounds [mjd2000].
+            - tof (``list``): Transfer time bounds [days].
+            - vinf_dep (``float``): allowed launch DV [km/s] 
+            - vinf_arr (``float``): allowed arrival DV [km/s]
+            - hf (``bool``): High-fidelity. Activates a continuous representation for the thrust.
+        """
+        self.args = defaults
+        for arg in self.args:
+            if arg in kwargs:
+                self.args[arg] = kwargs[arg]
+        self.w_mass = self.args["w_mass"]
+        self.w_tof = self.args["w_tof"]
+        # initialise base
+        _direct_base.__init__(self, mass, thrust, isp, nseg, pk.MU_SUN, hf)
+
+        # planets
+        if all([isinstance(pl, str) for pl in [p0, pf]]):
+            self.p0 = pk.planet.jpl_lp(p0)
+            self.pf = pk.planet.jpl_lp(pf)
+        else:
+            raise TypeError("Planet names must be supplied as str.")
+
+        # bounds TODO check
+        assert t0[1] - t0[0] >= tof[0]
+        assert all(t > 0 for t in tof)
+        assert tof[1] > tof[0]
+        self.t0 = t0
+        self.tof = tof
+
+        # boundary conditions on velocity
+        self.vinf_dep = vinf_dep * 1000  # (in m)
+        self.vinf_arr = vinf_arr * 1000  # (in m)
+
+        # The class is built around solar system planets hence mu is always the
+        # SUN
+        self.mu = pk.MU_SUN
+
+    def fitness(self, z):
+
+        # epochs (mjd2000)
+        t0 = pk.epoch(z[0])
+        tf = pk.epoch(z[0] + z[1])
+
+        # final mass
+        mf = z[2]
+
+        # controls
+        u = z[9:]
+
+        # compute Cartesian states of planets
+        r0, v0 = self.p0.eph(t0)
+        rf, vf = self.pf.eph(tf)
+
+        # add the vinfs from the chromosome
+        v0 = [a + b for a, b in zip(v0, z[3:6])]
+        vf = [a + b for a, b in zip(vf, z[6:9])]
+
+        # spacecraft states
+        x0 = pk.sims_flanagan.sc_state(r0, v0, self.sc.mass)
+        xf = pk.sims_flanagan.sc_state(rf, vf, mf)
+
+        # set leg
+        self.leg.set(t0, x0, u, tf, xf)
+
+        # compute equality constraints
+        ceq = np.asarray(self.leg.mismatch_constraints(), np.float64)
+
+        # nondimensionalise equality constraints
+        ceq[0:3] /= pk.AU
+        ceq[3:6] /= pk.EARTH_VELOCITY
+        ceq[6] /= self.sc.mass
+
+        # compute inequality constraints
+        cineq = np.asarray(self.leg.throttles_constraints(), np.float64)
+
+        # compute inequality constraints on departure and arrival velocities
+        v_dep_con = (z[3] ** 2 + z[4] ** 2 + z[5] ** 2 - self.vinf_dep ** 2)
+        v_arr_con = (z[6] ** 2 + z[7] ** 2 + z[8] ** 2 - self.vinf_arr ** 2)
+
+        # nondimensionalize inequality constraints
+        v_dep_con /= pk.EARTH_VELOCITY ** 2
+        v_arr_con /= pk.EARTH_VELOCITY ** 2
+
+        return np.hstack(([self.obj_func(z)], ceq, cineq, [v_dep_con, v_arr_con]))
+
+    def obj_func(self, z):
+        tof, mf, = z[1:3]
+        return -(self.w_mass*mf + self.w_tof*tof)
+    
+    def get_nic(self):
+        return super().get_nic() + 2
+
+    def get_bounds(self):
+        lb = [self.t0[0], self.tof[0], self.sc.mass * 0.1] + \
+            [-self.vinf_dep] * 3 + [-self.vinf_arr] * 3 + \
+            [-1, -1, -1] * self.nseg
+        ub = [self.t0[1], self.tof[1], self.sc.mass] + \
+            [self.vinf_dep] * 3 + [self.vinf_arr] * 3 + \
+            [1, 1, 1] * self.nseg
+        return (lb, ub)
+
+    def _plot_traj(self, z, axis, units):
+
+        # times
+        t0 = pk.epoch(z[0])
+        tf = pk.epoch(z[0] + z[1])
+
+        # plot Keplerian
+        pk.orbit_plots.plot_planet(
+            self.p0, t0, units=units, color=(0.8, 0.8, 0.8), axes=axis)
+        pk.orbit_plots.plot_planet(
+            self.pf, tf, units=units, color=(0.8, 0.8, 0.8), axes=axis)
+
+    def _pretty(self, z):
+        print("\nLow-thrust NEP transfer from " +
+              self.p0.name + " to " + self.pf.name)
+        print("\nLaunch epoch: {!r} MJD2000, a.k.a. {!r}".format(
+            z[0], pk.epoch(z[0])))
+        print("Arrival epoch: {!r} MJD2000, a.k.a. {!r}".format(
+            z[0] + z[1], pk.epoch(z[0] + z[1])))
+        print("Time of flight (days): {!r} ".format(z[1]))
+        print("\nLaunch DV (km/s) {!r} - [{!r},{!r},{!r}]".format(np.sqrt(
+            z[3]**2 + z[4]**2 + z[5]**2) / 1000, z[3] / 1000, z[4] / 1000, z[5] / 1000))
+        print("Arrival DV (km/s) {!r} - [{!r},{!r},{!r}]".format(np.sqrt(
+            z[6]**2 + z[7]**2 + z[8]**2) / 1000, z[6] / 1000, z[7] / 1000, z[8] / 1000))
+
+    @staticmethod
+    def _get_controls(z):
+        return z[9:]
---
+++ pl2pl_optim.py
@@ -0,0 +1,165 @@
+import argparse
+import logging
+import pykep.trajopt as tropt
+from pykep.examples import add_gradient, algo_factory
+import yaml
+import pygmo as pg
+import math
+import matplotlib.pyplot as plt
+import time
+from date_utils import mjd2000_to_date
+
+months = {1:"January",
+          2:"February",
+          3:"March",
+          4:"April",
+          5:"May",
+          6:"June",
+          7:"July",
+          8:"August",
+          9:"September",
+          10:"October",
+          11:"November",
+          12:"December"}
+
+dep = "earth"
+tgt = "mars"
+
+logger = logging.getLogger(__name__)
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+
+possible_models = ['BHT-1500']
+
+
+def load_engines(yaml_path):
+    engines = {}
+    with open(yaml_path, "r") as yaml_file:
+        yaml_content = yaml.load(yaml_file)
+        for engine in yaml_content:
+            engines[engine] = [mode_str.split() for mode_str in yaml_content[engine]]
+            # Clean modes
+            for mode in engines[engine]:
+                for i, spec in enumerate(mode):
+                    mode[i] = float(spec)
+    return engines
+        
+def print_args(args, et_models):
+    if args.model:
+        specs = et_models[args.model][args.mode]
+        engine = f"{args.model}, at Power: {specs[0]:.0f} W, Thrust: {specs[1]:.3f} N, Isp: {specs[2]:.0f} s" 
+    else:
+        p = args.thrust*args.isp*9.81*0.5/0.5 #50% efficiency
+        engine = f"Power: {p:.0f}W, Thrust: {args.thrust:.3f} N, Specific Impulse: {args.isp:.0f} s"
+    dep_date = mjd2000_to_date(args.t0[0])
+    arr_date = mjd2000_to_date(args.t0[1])
+    
+    print(f"-"*120 + "\n" + f"-"*120 + "\n" +
+          f"Spacecraft wet mass: {args.mass:.0f} kg \n"
+          f"Engine: {engine} \n"
+          f"Dearture: {args.vinf_dep} km/s on {math.floor(dep_date[2])} of {months[dep_date[1]]}, {dep_date[0]} \n"
+          f"Arrival: {args.vinf_arr} km/s on {math.floor(arr_date[2])} of {months[arr_date[1]]}, {arr_date[0]} \n"
+          f"Time of flight from {args.tof[0]} to {args.tof[1]} days"
+          + "\n" + f"-"*120 + "\n" + f"-"*120)
+        
+def main(args):
+    if args.model:
+        
+        logger.debug(f"Models available: {et_models}")
+        et_mode = et_models[args.model][args.mode]
+        logger.debug(f"You've chosen {args.model} at mode {et_mode}")
+        _, args.thrust, args.isp = et_mode
+        
+    algo = algo_factory("slsqp")
+    earth_to_mars = add_gradient(tropt.direct_pl2pl(p0=dep,
+                                       pf=tgt,
+                                       mass=args.mass,
+                                       thrust=args.thrust,
+                                       isp=args.isp,
+                                       nseg=args.nseg,
+                                       t0=args.t0,
+                                       tof=args.tof,
+                                       vinf_dep=args.vinf_dep,
+                                       vinf_arr=args.vinf_arr,
+                                       hf=False),
+                                with_grad=True)
+    prob = pg.problem(earth_to_mars)
+    prob.c_tol = [1e-5] * prob.get_nc()
+    pop = pg.population(prob, 1)
+    start = time.time()
+    pop = algo.evolve(pop)
+    logger.info(f"Time elapsed: {(time.time() - start):.3f}")
+    if prob.feasibility_x(pop.champion_x):
+        logger.info("OPTIMAL FOUND!")
+    else:
+        logger.info("No solution found, try again")
+     
+    earth_to_mars.udp_inner.pretty(pop.champion_x)
+     
+     
+    earth_to_mars.udp_inner.plot_traj(pop.champion_x)
+    plt.title("The trajectory in the heliocentric frame")
+    earth_to_mars.udp_inner.plot_control(pop.champion_x)
+    plt.title("The control profile (throttle)")
+ 
+    plt.show()
+
+if __name__ == "__main__":
+    
+    et_models = load_engines("engines.yaml")
+    
+    logger = logging.getLogger()
+    # PROGRAM
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--log_level", default="info", type=str,
+        help="Set to off, info, or debug to control verbosity"
+    )
+    # SPACECRAFT
+    spacecraft_options = parser.add_argument_group("Spacecraft options")
+    spacecraft_options.add_argument("--mass", type=float, default=420,
+                        help="Wet mass of the spacecraft upon TMI")
+    # ENGINE
+    engine_options = parser.add_argument_group("Engine options")
+    engine_options.add_argument("--thrust", type=float, default=0.102,
+                    help="Thrust during transfer")
+    engine_options.add_argument("--isp", type=float, default=1915,
+                    help="Specific impulse during transfer")
+    engine_options.add_argument("--model", default=None,
+                    help=f"Select a model amongst: {list(et_models.keys())}. Overrides thrust and isp settings")
+    engine_options.add_argument("--mode", type=int, default=0,
+                    help="Engine mode during tranfer. Necessary only if a model was selected. 0 is the default mode")
+    engine_options.add_argument("--list_modes", action='store_true',
+                    help="List available modes for the selected model. Will do that instead of running program")
+    # TRAJECTORY
+    trajectory_options = parser.add_argument_group("Trajectory options")
+    trajectory_options.add_argument("--t0", nargs=2, type=int, default=[9497, 10000],
+                                    help="Start and end bound dates for the start of the trajectory." +
+                                    " The optimization explores dates lying between the two.")
+    trajectory_options.add_argument("--tof", nargs=2, type=int, default=[200, 500],
+                                    help="Start and end bound dates for the time of flight. The optimization"
+                                    + "explores times of flight lying between the two.")
+    trajectory_options.add_argument("--vinf_dep", type=float, default=1e-3,
+                                    help="Allowed v infinity for Earth departure")
+    trajectory_options.add_argument("--vinf_arr", type=float, default=1e-3,
+                                    help="Allowed v infinity for Mars arrival")
+    # TRAJECTORY
+    optimizer_options = parser.add_argument_group("Optimizer options")
+    optimizer_options.add_argument("--nseg", type=int, default=20,
+                                    help="Number of calculation segments")
+    args = parser.parse_args()
+    
+    log_level = getattr(logging, args.log_level.upper())
+    logging.basicConfig(level=log_level)
+    logger.setLevel(log_level)
+    
+    if args.list_modes:
+        for i, mode in enumerate(et_models[args.model]):
+            eff = mode[1]*mode[2]*9.81*0.5/mode[0]*100
+            print(f"Mode {i:2d}: {mode[0]:.0f} W \t {mode[1]:.3f} N \t {mode[2]:.0f} s, Efficiency: {eff:.2f} %")
+        exit()
+    main(args)
+    print_args(args, et_models)
+    
+    
+    
+    
